6d4459b1a9d0a23f54cb21c26748a60c8821c6df
streamevolution <mrandroidtutorialeshd@gmail.com>
2026-08-13 09:17:07 -0600

    Correct comment typo and fix bracket placement
    Fix typo in comment and adjust closing bracket placement.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class ScrapingQueue {
     async enqueue(task, req) {
         let isCancelled = false;
         
-        // Si el usuario cierra la pestaña o la plataforma corta la conexión, lo marcamos
+        // Si eell usuario cierra la pestaña o la plataforma corta la conexión, lo marcamos
         if (req) {
             req.on('close', () => {
                 isCancelled = true;
